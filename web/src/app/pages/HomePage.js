b3f959a430c933adaf1d17815a1ee175676184ea
llfiji <shift0x00@gmail.com>
2025-06-18 18:38:40 -0400

    tidy up homepage

--- web/src/app/pages/HomePage.js
+++ web/src/app/pages/HomePage.js
@@ -7,7 +7,7 @@ const HomePage = () => {
       <header className="homepage-header">
         <div className="homepage-logo-nav">
           <div className="homepage-logo">
-            <img src="/crypto-bank.png" alt="CryptoBank Logo" className="homepage-logo-img" />
+            <img src="/logo.png" alt="CryptoBank Logo" className="homepage-logo-img" />
             <span className="homepage-logo-text">Infinita Bank</span>
           </div>
           <nav className="homepage-nav">
@@ -18,7 +18,7 @@ const HomePage = () => {
       <main className="homepage-main">
         <section className="homepage-hero-text">
           <h1 className="homepage-title">Real World Loans for Infinita Citizens</h1>
-          <p className="homepage-subtitle">Unlock capital for real world projects in infinita via under collateralized loans</p>
+          <p className="homepage-subtitle">Unlock capital for real world projects in infinita with lending powered by crypto</p>
           <button className="button-dark">Get Started</button>
         </section>
         <section className="homepage-hero-image">
